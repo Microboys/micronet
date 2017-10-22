179010bde4b7c667689d6b218039b1b0ea609a35
Rishabh Jain <wakeuprj@gmail.com>
2017-10-22 16:16:31 +0100

    Added flash script for mac.

---
+++ router/flashMac.sh
@@ -0,0 +1,2 @@
+cp build/$(yt --plain target | head -n 1 | cut -f 1 -d' ')/source/$(yt --plain ls | head -n 1 | cut -f 1 -d' ')-combined.hex /Volumes/MICROBIT
+cp build/$(yt --plain target | head -n 1 | cut -f 1 -d' ')/source/$(yt --plain ls | head -n 1 | cut -f 1 -d' ')-combined.hex /Volumes/MICROBIT\ 1
