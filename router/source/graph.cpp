4b3abb360ebf3f115cb19da0c3758a1a2570b858
Vinamra Agrawal <va1215@ic.ac.uk>
2017-10-31 17:57:24 +0000

    added Dijkstra’s routing algorithm

--- router/source/graph.cpp
+++ router/source/graph.cpp
@@ -44,6 +44,91 @@ void delete_extra_neighbours(uint16_t ip) {
   }
 }
 
+// <node, path node>
+// <node, distance>
+// <node, boolean computation done>
+std::unordered_map<uint16_t,uint16_t> path_for_node;
+std::unordered_map<uint16_t,int> distance_for_node;
+std::unordered_map<uint16_t,bool> if_computed;
+
+void calculate_syn_tree(uint16_t source) {
+
+    distance_for_node[source] = 0;
+
+    //get min distance and edge for next iteration
+    uint16_t min_edge = 0;
+
+    //get distance and write path for neighbours
+    for (auto it : graph) {
+        struct edge cur_edge = it.first;
+        if (cur_edge.from == source) {
+            int distance = abs(it.second);
+            distance_for_node[cur_edge.to] = distance;
+            path_for_node[cur_edge.to] = cur_edge.to;
+        }
+    }
+    if_computed[source] = true;
+
+    //write iterative for rest
+    while(true){
+      min_edge = find_min_edge();
+      if (min_edge == 0){
+        //loop complete
+        break;
+      }
+
+      for (auto it : graph) {
+        struct edge cur_edge = it.first;
+          if (cur_edge.from == min_edge) {
+              int distance = abs(it.second) + distance_for_node[min_edge];
+
+              // if value not initialized in the graph
+              auto it = distance_for_node.find(cur_edge.to);
+              if (it == distance_for_node.end()){
+                distance_for_node[cur_edge.to] = INT_MAX;
+                if_computed[cur_edge.to] = false;
+              }
+
+              if (distance < distance_for_node[cur_edge.to]){
+                distance_for_node[cur_edge.to] = distance;
+                path_for_node[cur_edge.to] = path_for_node[min_edge];
+              }
+          }
+       }
+       if_computed[min_edge] = true;
+    }
+}
+
+uint16_t find_min_edge() {
+  int min_distance = INT_MAX;
+  uint16_t min_edge = 0;
+
+  for (auto it : distance_for_node) {
+      if (it.second <= min_distance && !if_computed[it.first]) {
+          min_edge = it.first;
+      }
+  }
+  return min_edge;
+}
+
+bool if_all_nodes_computed(std::unordered_map<uint16_t,bool> if_computated){
+  for (auto it : if_computated) {
+      if (!it.second) {
+          return false;
+      }
+  }
+  return true;
+}
+
+int16_t get_node_for_path(int16_t target){
+  for (auto it : path_for_node) {
+      if (it.first == target) {
+          return it.second;
+      }
+  }
+  return 0;
+}
+
 void delete_all_edges(uint16_t ip) {
     for (auto it : graph) {
         struct edge cur_edge = it.first;
--- router/source/graph.h
+++ router/source/graph.h
@@ -29,6 +29,8 @@ void update_graph(Packet* p);
 void update_graph(uint16_t from, uint16_t to, int distance);
 void delete_all_edges(uint16_t ip);
 void delete_extra_neighbours(uint16_t ip);
+bool if_all_nodes_computed(std::unordered_map<uint16_t,bool> if_computated);
+uint16_t find_min_edge();
 void print_graph(MicroBitSerial serial);
 void print_graph(MicroBitSerial serial, std::unordered_map<edge, int> graph);
 std::unordered_map<edge, int> get_lsa_graph(uint16_t ip);
