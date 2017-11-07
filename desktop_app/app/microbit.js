e4aa5091c87227b1470c3f1967f2056c714fee5e
danrobdean <dd2415@ic.ac.uk>
2017-11-07 19:25:32 +0000

    Add visual IDs based on ID num in graph.
    Doesn't currently work in packaged mode due to image dependency shenanigans but works well in develop mode.

--- desktop_app/app/microbit.js
+++ desktop_app/app/microbit.js
@@ -1,12 +1,20 @@
 import SerialPort from 'serialport';
 import graphActions from './actions/graph';
 
+var fs = require('electron').remote.require('fs');
+var Jimp = require("jimp");
+
 const microbitProductId = "0204";
 const microbitVendorId = "0d28";
 const microbitBaudRate = 115200;
 
 const minLength = 140;
 const lengthCoeff = 2;
+
+const imgLEDX = 142;
+const imgLEDY = 114;
+const imgOffsetX = 33;
+const imgOffsetY = 33;
 
 var microbitPort = null;
 var locating = false;
@@ -69,7 +77,6 @@ function transformGraphJSON(graphJSON) {
     var nodes = [];
     var edges = [];
     var ip = obj.ip;
-    console.log("Connected micro:bit: " + ip);
     for (var i = 0; i < graph.length; i++) {
       var arc = graph[i];
       addNode(nodes, arc.from, ip);
@@ -86,14 +93,49 @@ function transformGraphJSON(graphJSON) {
 }
 
 function addNode(nodes, id, ip) {
-  var obj = {id: id, label: "Node " + id};
+  var node = {id: id, label: "Node " + id};
   if (ip == id) {
-    obj.shadow = {enabled: true, color: "#59B4FF", x: 0, y: 0, size: 20};
-    obj.label += " (connected)"
+    node.shadow = {enabled: true, color: "#59B4FF", x: 0, y: 0, size: 20};
+    node.label += " (connected)"
   }
+  node.image = generateMicrobitImage(id);
   if (!nodeExists(nodes, id)) {
-    nodes.push(obj);
+    nodes.push(node);
   }
+}
+
+//Generates an image of the microbit showing the visual ID derived from code
+//code = denary of ID, converted to binary gives length 25 bin string - each bit
+//represents 1 LED (e.g. code = '2' = 0b10 = 000...0010, second last LED is lit)
+function generateMicrobitImage(code) {
+  //First check if image exists for this ID
+  var codeImgPath = 'assets/microbit-' + code + '.png';
+  var imgPath = codeImgPath;
+  try {
+    fs.readdir('.', (err, items) => {console.log(items);})
+    fs.accessSync('./build/' + imgPath);
+  } catch(err) {
+    //Image doesn't exist, create using Jimp and use default img for now
+    imgPath = 'assets/microbit.png';
+    Jimp.read('./build/assets/microbit.png', (err, microbit) => {
+      if (err) {
+        throw err;
+      }
+      Jimp.read('./build/assets/led.png', (err, led) => {
+        if (err) {
+          throw err;
+        }
+        var newImg = microbit.clone();
+        var codeBin = parseInt(code).toString(2);
+        for (var i = 24; i > 24 - codeBin.length; i--) {
+          if (codeBin[codeBin.length - (25 - i)] == '1') newImg.blit(led, imgLEDX + (i % 5) * imgOffsetX, imgLEDY + Math.floor(i / 5) * imgOffsetY);
+        }
+        newImg.write('./build/' + codeImgPath);
+      })
+    });
+  }
+
+  return './' + imgPath;
 }
 
 function nodeExists(nodes, id) {
