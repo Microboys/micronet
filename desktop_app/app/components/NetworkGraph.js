cb3000c2fcf81b49e09a86cfc27d1cd689f20370
danrobdean <dd2415@ic.ac.uk>
2017-11-13 22:48:49 +0000

    Display message modal on node double-click.

--- desktop_app/app/components/NetworkGraph.js
+++ desktop_app/app/components/NetworkGraph.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Graph from 'react-graph-vis';
+import { Modal, ModalHeader, ModalBody, ModalFooter, Input, Form, Button } from 'reactstrap';
+import { sendMsg } from './../microbit.js';
 
 export default class NetworkGraph extends Component {
   
@@ -27,8 +29,40 @@ export default class NetworkGraph extends Component {
           }
         }
       },
-      events: {}
+      events: {
+        doubleClick: (event) => {
+          if (event.nodes.length != 1) return;
+          var node = event.nodes[0];
+          this.setState({modal: !this.state.modal, selectedNode: node});
+        }
+      },
+      modal: false,
+      selectedNode: "",
+      msg: "",
+      msgId:'msgInput'
     };
+    this.handleChangeMsg = this.handleChangeMsg.bind(this);
+    this.handleSend = this.handleSend.bind(this);
+    this.toggleModal = this.toggleModal.bind(this);
+    this.focusMsg = this.focusMsg.bind(this);
+  }
+
+  toggleModal() {
+    this.setState({modal: !this.state.modal, msg: ""});
+  }
+
+  handleChangeMsg(event) {
+    this.setState({msg: event.target.value});
+  }
+
+  handleSend(event) {
+    sendMsg(this.state.to, this.state.msg);
+    event.preventDefault();
+    this.toggleModal();
+  }
+
+  focusMsg() {
+    this.msgInput.focus();
   }
 
   render() {
@@ -38,7 +72,20 @@ export default class NetworkGraph extends Component {
       );
     } else {
       return (
-        <Graph graph={this.props.graph} options={this.state.options} events={this.state.events} />
+        <div className='fullSize'>
+          <Modal className={this.props.className} isOpen={this.state.modal} toggle={this.toggleModal} fade={false} onEnter={this.focusMsg}>
+            <ModalHeader toggle={this.toggleModal}>Send Message To {this.state.selectedNode}</ModalHeader>
+            <ModalBody>
+              <Form onSubmit={this.handleSend}>
+                <Input id={this.msgId} onChange={this.handleChangeMsg} value={this.state.msg} getRef={(input) => {this.msgInput = input;}}/>
+              </Form>
+            </ModalBody>
+            <ModalFooter>
+              <Button onClick={this.handleSend}>Send Message</Button>
+            </ModalFooter>
+          </Modal>
+          <Graph graph={this.props.graph} options={this.state.options} events={this.state.events} />
+        </div>
       );
     }
   }
