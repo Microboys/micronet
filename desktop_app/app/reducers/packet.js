c06f4dc578a7ec349998e4ddda0cbdb102b26ae9
Nik Gupta <vngupta77@gmail.com>
2017-11-16 21:39:51 +0000

    Set max capacity on packet view.

--- desktop_app/app/reducers/packet.js
+++ desktop_app/app/reducers/packet.js
@@ -3,11 +3,12 @@ import actions from '../actions/packet.js';
 
 export default handleActions({
   [actions.addPacket]: (state, action) => {
-    return { ...state, "recieved" : state.received.push(action.payload) };
-  },
-  [actions.removeOldestPacket]: (state, action) => {
-    console.log("Reducing Reducing Reducing");
-    return { ...state, "recieved" : state.received.slice(0, state.received.length) };
+    let received = state.received;
+    if (received.length == state.capacity) {
+      received = received.slice(1, received.length);
+    }
+    received.push(action.payload);
+    return { ...state, "received" : received };
   },
   [actions.removeAllPackets]: (state, action) => {
     return { ...state, "received" : [] };
