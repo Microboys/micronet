c657ca069c4e126d619423c7cbed0bda01f0bb3a
Rishabh Jain <wakeuprj@gmail.com>
2017-11-23 13:17:05 +0000

    Fixed flash file for mac.

--- router/flash-mac.sh
+++ router/flash-mac.sh
@@ -1,4 +1,3 @@
-cp build/$(yt --plain target | head -n 1 | cut -f 1 -d' ')/source/$(yt --plain ls | head -n 1 | cut -f 1 -d' ')-combined.hex /Volumes/MICROBIT
-cp build/$(yt --plain target | head -n 1 | cut -f 1 -d' ')/source/$(yt --plain ls | head -n 1 | cut -f 1 -d' ')-combined.hex /Volumes/MICROBIT\ 1
-cp build/$(yt --plain target | head -n 1 | cut -f 1 -d' ')/source/$(yt --plain ls | head -n 1 | cut -f 1 -d' ')-combined.hex /Volumes/MICROBIT\ 2
-cp build/$(yt --plain target | head -n 1 | cut -f 1 -d' ')/source/$(yt --plain ls | head -n 1 | cut -f 1 -d' ')-combined.hex /Volumes/MICROBIT\ 3
+for d in /Volumes/MICR* ; do
+    cp build/$(yt --plain target | head -n 1 | cut -f 1 -d' ')/source/$(yt --plain ls | head -n 1 | cut -f 1 -d' ')-combined.hex "$d";
+done
