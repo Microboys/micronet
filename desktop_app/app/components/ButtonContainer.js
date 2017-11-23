d5722486662ab65b13f3bd3cfda79db39c092e91
danrobdean <dd2415@ic.ac.uk>
2017-11-23 19:21:04 +0000

    Add flash button to searching page, refactor.

--- desktop_app/app/components/ButtonContainer.js
+++ desktop_app/app/components/ButtonContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Button, Row, Col } from 'reactstrap';
-import { flashMicrobit } from '../microbit.js'
+import FlashButton from './FlashButton'
+import { flashMicrobit } from '../microbit.js';
 import PropTypes from 'prop-types';
 
 export default class ButtonContainer extends Component {
@@ -21,7 +22,7 @@ export default class ButtonContainer extends Component {
           <Button id='renameBtn' onClick={this.props.showRenameModal} className='controlButton'>Rename Microbit</Button>
         </Row>
         <Row className='noMarginRow justify-content-centre'>
-          <Button id='flashBtn' onClick={flashMicrobit} className='controlButton'>Flash Microbit</Button>
+          <FlashButton id='flashBtn' className='controlButton' />
         </Row>
         <Row className='noMarginRow justify-content-centre'>
           <Button id='helpBtn' onClick={this.props.onHelp} className='controlButton'>Help</Button>
