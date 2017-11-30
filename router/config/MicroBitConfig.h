538a48176555a8a51ac76ce031e88816e8e809a3
Rishabh Jain <wakeuprj@gmail.com>
2017-11-30 15:28:40 +0000

    Changed nested heap size back to 0.75.

--- router/config/MicroBitConfig.h
+++ router/config/MicroBitConfig.h
@@ -79,10 +79,10 @@ DEALINGS IN THE SOFTWARE.
 
 // Defines where in memory persistent data is stored.
 #ifndef KEY_VALUE_STORE_PAGE
-#define KEY_VALUE_STORE_PAGE	                (PAGE_SIZE * (NRF_FICR->CODESIZE - 17))	
+#define KEY_VALUE_STORE_PAGE	                (PAGE_SIZE * (NRF_FICR->CODESIZE - 17))
 #endif
 
-#ifndef BLE_BOND_DATA_PAGE 
+#ifndef BLE_BOND_DATA_PAGE
 #define BLE_BOND_DATA_PAGE                      (PAGE_SIZE * (NRF_FICR->CODESIZE - 18))
 #endif
 
@@ -116,7 +116,7 @@ extern uint32_t __etext;
 
 // The proportion of SRAM available on the mbed heap to reserve for the micro:bit heap.
 #ifndef MICROBIT_NESTED_HEAP_SIZE
-#define MICROBIT_NESTED_HEAP_SIZE               0
+#define MICROBIT_NESTED_HEAP_SIZE               0.75
 #endif
 
 // If defined, reuse any unused SRAM normally reserved for SoftDevice (Nordic's memory resident BLE stack) as heap memory.
@@ -367,7 +367,7 @@ extern uint32_t __etext;
 // Defines the logical block size for the file system.
 // Must be a factor of the physical PAGE_SIZE (ideally a power of two less).
 //
-#ifndef MBFS_BLOCK_SIZE		
+#ifndef MBFS_BLOCK_SIZE
 #define MBFS_BLOCK_SIZE		256
 #endif
 
@@ -377,7 +377,7 @@ extern uint32_t __etext;
 // Should be <= MBFS_BLOCK_SIZE.
 //
 #ifndef MBFS_CACHE_SIZE
-#define MBFS_CACHE_SIZE	    0	
+#define MBFS_CACHE_SIZE	    0
 #endif
 
 //
